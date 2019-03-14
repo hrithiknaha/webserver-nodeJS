a96b12b231cc1b04e1828878657b453c462a2807
Hrithik Naha <naha.hrithkk@gmail.com>
2019-03-14 12:45:36 +0530

    Add Logger middle ware and Maintaince middleware

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const hbs = require("hbs");
+const fs = require("fs");
 
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+
 app.set("view engine", hbs);
+
+app.use(function(req,res,next){
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', function(err){
+        if(err){
+            console.log("Can't be accessed right now.")
+        }
+    });
+    next();
+})
+
+//Maintainance Path
+// app.use(function(req,res){
+//     res.render('maintainance.hbs')
+// });                  
+
 app.use(express.static(__dirname + "/public"));
+
 hbs.registerHelper('getCurrentYear', function(){
     return new Date().getFullYear();
 })
